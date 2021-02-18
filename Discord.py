00dbf55edcec6a00dc8fb06aff57a9093e883c0e
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-02-18 17:38:36 +0530

    Alias added

--- Discord.py
+++ Discord.py
@@ -14,6 +14,13 @@ try:
         for i in search(text,num=10,stop=10,pause=0):
             li=li+i+" \n"
         await ctx.send(li)
+    @client.command()
+    async def g(ctx,*,text):        
+        print(text)
+        li=""
+        for i in search(text,num=10,stop=10,pause=0):
+            li=li+i+" \n"
+        await ctx.send(li)
     client.run("ODExNTkxNjIzMjQyMTU0MDQ2.YC0bmQ.4oW1hyppcaQJpRfKFRJCiddZ5aI")
 except:
     print("Something has occured")
