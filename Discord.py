77f834d14653a2b9d547b538ac8e89024a99c3c4
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-02-18 19:23:29 +0530

    Help command added

--- Discord.py
+++ Discord.py
@@ -8,19 +8,31 @@ try:
         print("Prepared")
 
     @client.command()
-    async def google(ctx,*,text):        
-        print(text)
-        li=""
-        for i in search(text,num=10,stop=10,pause=0):
-            li=li+i+" \n"
-        await ctx.send(li)
-    @client.command()
     async def g(ctx,*,text):        
         print(text)
         li=""
         for i in search(text,num=10,stop=10,pause=0):
             li=li+i+" \n"
         await ctx.send(li)
+    @client.command()
+    async def google(ctx,*,text):        
+        print(text)
+        li=""
+        for i in search(text,num=10,stop=10,pause=0):
+            li=li+i+" \n"
+        await ctx.send(li)
+    client.remove_command("help")
+    @client.group(invoke_without_command=True)
+    async def help(ctx):
+        text="'google <text to search> \n'help to get this screen \n\nAlias: \n'g <text to search>"
+        em=discord.Embed(title="**HELP** \n",description=text,color=ctx.author.color)
+        await ctx.send(embed=em)
+    @client.group(invoke_without_command=True)
+    async def h(ctx):
+        text="'google <text to search> \n'help to show this message \n\nAlias: \n'g <text to search> \n'h to show this message"
+        em=discord.Embed(title="**HELP** \n",description=text,color=ctx.author.color)
+        await ctx.send(embed=em)   
     client.run("ODExNTkxNjIzMjQyMTU0MDQ2.YC0bmQ.4oW1hyppcaQJpRfKFRJCiddZ5aI")
 except:
     print("Something has occured")
+
