27e266e5968e1f895aaa074c5888ba91f243e1a3
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-02-18 20:11:29 +0530

    Reduced the links produced

--- Discord.py
+++ Discord.py
@@ -11,14 +11,14 @@ try:
     async def g(ctx,*,text):        
         print(text)
         li=""
-        for i in search(text,num=10,stop=10,pause=0):
+        for i in search(text,num=7,stop=7,pause=0):
             li=li+i+" \n"
         await ctx.send(li)
     @client.command()
     async def google(ctx,*,text):        
         print(text)
         li=""
-        for i in search(text,num=10,stop=10,pause=0):
+        for i in search(text,num=7,stop=7,pause=0):
             li=li+i+" \n"
         await ctx.send(li)
     client.remove_command("help")
