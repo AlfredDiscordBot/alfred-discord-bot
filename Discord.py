979219c79b5e257fa63349d01bde6ab2df8d2ba0
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-03-11 08:37:27 +0530

    Added some admin features

--- Discord.py
+++ Discord.py
@@ -1,15 +1,17 @@
 import discord
 from discord.ext import commands
 from googlesearch import search
-from math import *
-from statistics import *
-try:
+import math as ma
+import statistics as s
+
+if True:
     client=commands.Bot(command_prefix="'")
     @client.event
     async def on_ready():
         print("Prepared")
+    censor=[] 
     da={}
-    re=[0]
+    re=[0]    
     @client.command()
     async def check(ctx):
         req()
@@ -21,18 +23,8 @@ try:
         req()
         print("yey")
         em=discord.Embed(title="*yey*")
-        await ctx.send(embed=em)
-    @client.command()
-    async def g(ctx,*,text):
-        req()
-        print(text)
-        li="**"+text+"** \n\n"
-        for i in search(text,num=7,stop=7,pause=0):
-            li=li+i+" \n\n"
-        text=text.replace(' ','%20')
-        li=li+"**Query link:**https://www.google.com/search?q="+text+"\n"
-        await ctx.send(li)
-    @client.command()
+        await ctx.send(embed=em)    
+    @client.command(aliases=['g'])
     async def google(ctx,*,text):
         req()
         print(text)
@@ -41,19 +33,25 @@ try:
             li=li+i+" \n\n"
         text=text.replace(' ','%20')
         li=li+"**Query link:**https://www.google.com/search?q="+text+"\n"
-        await ctx.send(li)
-    @client.command()
-    async def m(ctx,*,text):
-        req()
-        a=eval(text)
-        text=text.replace("ma.","")
-        text=text.replace("s.","")        
-        print(text)
-        em=discord.Embed(title=text,description=text+"="+str(a),color=ctx.author.color)
-        await ctx.send(embed=em)
-    @client.command()
+        await ctx.send(li)    
+    @client.command(aliases=['cen'])
+    async def add_censor(ctx,*,text):
+    	string=""
+    	censor.append(text.lower())
+    	for i in range(0,len(text)):
+    		string=string+"-"
+    	em=discord.Embed(title="Added "+string+" to the list",decription="Done",color=ctx.author.color)
+    	await ctx.send(embed=em)
+    @client.event
+    async def on_message(msg):
+    	for word in censor:
+    		if word in msg.content.lower():
+    			await msg.delete()    	
+    	await client.process_commands(msg)
+    @client.command(aliases=['m'])
     async def meth(ctx,*,text):
         req()
+        pi=ma.pi
         a=eval(text)
         text=text.replace("ma.","")
         text=text.replace("s.","")        
@@ -67,9 +65,9 @@ try:
         em=discord.Embed(title="Requests",description=str(number),color=ctx.author.color)
         await ctx.send(embed=em)
     def r(x):
-        return radians(x)
+        return ma.radians(x)
     def d(x):
-        return degrees(x)
+        return ma.degrees(x)
     def add(p1,p2):
         da[p1]=p2
         return "Done"
@@ -128,16 +126,14 @@ try:
             root1 = (-b + determinant) / (2 * a)
             root2 = (-b - determinant) / (2 * a)
             return "This equation has two roots: "+str(root1)+","+str(root2)
-
-        
-        
+	
         
     @client.command()
     async def p(ctx,*,text):
         req()
         print("P"+text)
         a=eval(text)
-        ans=factorial(a[0])/factorial(a[0]-a[1])
+        ans=ma.factorial(a[0])/ma.factorial(a[0]-a[1])
         em=discord.Embed(title="P"+text+":",description=str(ans),color=ctx.author.color)
         await ctx.send(embed=em)
     @client.command()
@@ -145,14 +141,26 @@ try:
         req()
         print("c"+text)
         a=eval(text)
-        ans=factorial(a[0])/(factorial(a[1])*factorial(a[0]-a[1]))
+        ans=ma.factorial(a[0])/(ma.factorial(a[1])*ma.factorial(a[0]-a[1]))
         em=discord.Embed(title="C"+text+":",description=str(ans),color=ctx.author.color)
         await ctx.send(embed=em)
-    
+    @client.command(aliases=['mu'])
+    @commands.has_permissions(kick_members=True)
+    async def mute(ctx,member:discord.Member):
+    	m=ctx.guild.get_role(818839599249752066)
+    	await member.add_roles(m)
+    	await ctx.send(member.mention+" muted")    
+    @client.command(aliases=['um'])
+    @commands.has_permissions(kick_members=True)
+    async def unmute(ctx,member:discord.Member):
+    	m=ctx.guild.get_role(818839599249752066)
+    	await member.remove_roles(m)
+    	await ctx.send(member.mention+" unmuted")
+    	
     te="**Commands**\n'google <text to search> \n'help to get this screen\n'c (n,r) for *combination* \n'p (n,r) for *permutation* \n**Leave space between p/c and the bracket'('** \n'meth <Expression> for any math calculation *(includes statistic)*\n'get_req for no. of requests\n"
-    te=te+"\n\nr(angle in degree) to convert angle to radian \nd(angle in radian) to convert angle to radian\n\n"
+    te=te+"**Modules**:\n**ma** for math module\n**s** for statistics module \n\nr(angle in degree) to convert angle to radian \nd(angle in radian) to convert angle to radian\n\n"
     te=te+"**Alias**: \n'g <text to search> \n'h to show this message \n'm <Expression> for any math calculation *(includes statistic)*\n\n"
-    te=te+"**Example**:\n'm quad('4x^2+2x-3')\n'p (10,9) \n'm sin(r(45))\n'm cos(pi)\n'help\nlcm(<integer seperated by comma>)\n**Use small letters only**"
+    te=te+"**Example**:\n'm quad('4x^2+2x-3')\n'p (10,9) \n'm ma.sin(r(45))\n'm ma.cos(pi)\n'help\n**Use small letters only**"
     client.remove_command("help")
     @client.group(invoke_without_command=True)
     async def help(ctx):
@@ -167,6 +175,5 @@ try:
         em=discord.Embed(title="**HELP** \n",description=te,color=ctx.author.color)
         await ctx.send(embed=em)   
     client.run("ODExNTkxNjIzMjQyMTU0MDQ2.YC0bmQ.4oW1hyppcaQJpRfKFRJCiddZ5aI")
-except:
+else:
     print("Something has occured")
-
