2fa6cd0cb111d22202ab3175bde3892b11fde81c
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2021-03-18 19:05:36 +0530

    Updated key

--- Discord.py
+++ Discord.py
@@ -606,7 +606,7 @@ if True:
         print("help")
         em=discord.Embed(title="**HELP** \n",description=te,color=ctx.author.color)
         await ctx.send(embed=em)
-    client.run(str(os.environ['App_key']))
+    client.run(os.environ['App_key'])
     
 else:
     print("Something has occured")
