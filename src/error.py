aac91463274fcf582e5a89c65c680bce9c606f8d
Yash Pawar <yashpawarp@gmail.com>
2021-09-11 23:04:31 +0530

    added error msg embed

---
+++ src/error.py
@@ -0,0 +1,14 @@
+import discord 
+
+def error(message):
+    embed = discord.Embed(
+        color = discord.Color.from_rgb(),
+        thumbnail = "https://github.com/yashppawar/alfred-discord-bot/blob/replit/error.png?raw=true",
+        message = str(message)
+    )
+
+def requirements():
+    return ''
+
+def main(client):
+    pass
