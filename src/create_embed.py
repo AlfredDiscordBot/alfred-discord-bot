f8b5beeaec0b2bad6807f7f1ba31254d4a11d203
Yash Pawar <yashpawarp@gmail.com>
2021-09-20 21:48:17 +0530

    🛠  fix print statement for yml embed

--- src/create_embed.py
+++ src/create_embed.py
@@ -145,9 +145,10 @@ def embed_from_yaml(yaml: str, ctx_author) -> discord.Embed:
     """
     info = safe_load(yaml)
     info = {k.lower():v for k,v in info.items()}
-    
+
     info['color'] = get_color(info.get('color', None))
-    print(info)
+    # print(info)
+    print(f"Creating Emebed for: '{ctx_author.name}' aka '{ctx_author.nick}' in '{ctx_author.guild}'")
 
     embed = discord.Embed(**info)
     
