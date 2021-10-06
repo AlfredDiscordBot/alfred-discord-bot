82c80cb0e6ff12bc75c195b94405f2bb8de9a318
Yash Pawar <yashpawarp@gmail.com>
2021-10-06 21:53:25 +0530

    add docstrings to error_logger

--- src/error_logger.py
+++ src/error_logger.py
@@ -1,18 +1,37 @@
 import discord 
 import traceback
+from discord.channel import TextChannel
+from discord.ext.commands.context import Context
 from functools import wraps
-from typing import Callable
+from typing import Callable, Union, Awaitable
 
 
 class ErrorLogger:
-    def __init__(self, dev_channel: discord.channel.TextChannel):
+    """
+    ErrorLogger: error logging utility class
+    provides with few methods and decorators to track the error 
+
+    :param dev_channel: TextChannel to send the error emebd
+    """
+    def __init__(self, dev_channel: Union[TextChannel, None]) -> None:
         self.set_channel(dev_channel)
     
-    def set_channel(self, dev_channel: discord.channel.TextChannel):
+    def set_channel(self, dev_channel: Union[TextChannel, None]) -> None:
+        """
+        Set the Log channel to the Dev Channel
+        
+        :param dev_channel: TextChannel to send the error embed
+        """
         self.channel = dev_channel
     
     @staticmethod
-    def get_embed(error_traceback: str, guild_name: str):
+    def get_embed(error_traceback: str, guild_name: str) -> discord.Embed:
+        """
+        Create Embed using the error_traceback and guild_name.
+
+        :param str error_traceback: the tracback for the error.
+        :param guild_name: the name of the server in which the error was recorded.
+        """
         embed = discord.Embed(
             color = discord.Color.from_rgb(240, 196, 25),
             description = f"```py\n{str(error_traceback)}\n```",
@@ -24,13 +43,25 @@ class ErrorLogger:
 
         return embed
     
-    async def log(self, error_traceback: str, guild_name: str):
+    async def log(self, error_traceback: str, guild_name: str) -> None:
+        """
+        Log the error_traceback in the dev_channel
+
+        :param str error_traceback: the tracback for the error.
+        :param guild_name: the name of the server in which the error was recorded.
+        """
         embed = self.get_embed(error_traceback, guild_name)
         await self.channel.send(embed=embed)
 
-    def track_command(self, function: Callable):
+    def track_command(self, function: Callable) -> Awaitable:
+        """
+        track the errors of the command function wrapped by track_command decorator
+        
+        :param function: command function to track.
+        """
+        
         @wraps(function)
-        async def wrapper(ctx, *args, **kwargs):
+        async def wrapper(ctx: Context, *args, **kwargs):
             try:
                 await function(ctx, *args, **kwargs)
             except:
