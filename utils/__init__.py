b933a8ab3a78107762d1dbd993a37da5378aab83
alvinbengeorge <alvinallen333@gmail.com>
2022-08-22 19:57:00 +0530

    🎨 format

--- utils/__init__.py
+++ utils/__init__.py
@@ -21,7 +21,7 @@ class DEVOP:
         "footer": {
             "text": "Only Developers can acces this function\nHave a good day Master Wayne",
             "icon_url": "https://cdn.discordapp.com/avatars/811591623242154046/18736ae6885bad04990795dff7acf2ad.png?width=664&height=664",
-        }
+        },
     }
 
 
@@ -37,6 +37,5 @@ async def send_devop(CLIENT: ef.commands.Bot, channel: int, functions: dict):
         [i async for i in channel.history(limit=100) if not i.pinned][:100]
     )
     return await channel.send(
-        embed=ef.cembed(**embed),
-        view=assets.DEVOPVIEW(CLIENT, functions=functions)
+        embed=ef.cembed(**embed), view=assets.DEVOPVIEW(CLIENT, functions=functions)
     )
