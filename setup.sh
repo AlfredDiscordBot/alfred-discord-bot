56bac11ac890547c59e26e1e68f4b39b45a7577d
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2022-08-29 06:27:40 +0000

    Removing lava extension from nextcord

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 [ -d "lava/" ] && rm -rf lava/;
 git clone "https://github.com/nextcord-ext/lava";
+rm -rf "/home/runner/alfred-discord-bot/venv/lib/python3.8/site-packages/nextcord/ext/lava/"
 mv "lava/nextcord/ext/lava/" "/home/runner/alfred-discord-bot/venv/lib/python3.8/site-packages/nextcord/ext/lava/";
 rm -rf lava/
 nohup java -jar Lavalink.jar &
